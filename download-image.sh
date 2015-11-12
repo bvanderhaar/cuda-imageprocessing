07537e5435eb7dd1a9723b2451cfd485e82cc301
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-11 21:37:20 -0500

    add download script

---
+++ download-image.sh
@@ -0,0 +1,4 @@
+curl -O http://www.cis.gvsu.edu/~wolffe/courses/cs677/projects/sestonDiatom.bmp
+curl -O http://www.cis.gvsu.edu/~wolffe/courses/cs677/projects/yeast.bmp
+curl -O http://www.cis.gvsu.edu/~wolffe/courses/cs677/projects/chryseobacterium.bmp
+curl -O http://www.cis.gvsu.edu/~wolffe/courses/cs677/projects/mycoplasma.bmp
