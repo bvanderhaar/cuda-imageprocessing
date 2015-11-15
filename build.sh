35fb4be38e039996cee752fb54123375316b2bec
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-15 14:47:39 -0500

    proper ignores

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-g++ sobel-cpu.cpp -o image-filter
+g++ sobel-cpu.cpp -o cpu-image-filter
 chmod +x image-filter
 if hash nvcc 2>/dev/null; then
-  nvcc sobel.cpp sobel.cu
+  nvcc sobel.cpp sobel.cu -o gpu-image-filter
 fi
