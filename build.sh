063e1ad4d7964a4f1583b80ade3a43f56a8c4d3f
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-15 17:04:48 -0500

    c++11 proper

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ fi
 g++ -std=c++1y -pedantic sobel-cpu.cpp -o cpu-image-filter
 chmod +x cpu-image-filter
 if hash nvcc 2>/dev/null; then
-  nvcc -std=c++0x sobel.cpp sobel.cu -o gpu-image-filter
+  nvcc --std=c++11 sobel.cpp sobel.cu -o gpu-image-filter
 fi
