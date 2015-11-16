d5d89018e0cebe4586bb6a67b0fe90c1b1ab79a5
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-15 23:40:52 -0500

    fix row/col

--- sobel.cu
+++ sobel.cu
@@ -41,7 +41,7 @@ __global__ void cu_sobel(int *l_source_array_d, int *l_result_array_d, int rows,
     sum_0 = (x_0 + (2 * x_1) + x_2) - (x_6 + (2 * x_7) + x_8);
     sum_1 = (x_2 + (2 * x_5) + x_8) - (x_0 + (2 * x_3) + x_6);
     // write new data onto smaller matrix
-    l_result_array_d[(col - 1) * (grid_width - 2) + (row - 1)] = sum_0 + sum_1;
+    l_result_array_d[(row - 1) * (grid_width - 2) + (col - 1)] = sum_0 + sum_1;
   }
 }
 
