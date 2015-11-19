dace734a33f73e479e0fa398571a5ec510b02854
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-18 20:30:29 -0500

    add linear prog

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ if hash clang-format 2>/dev/null; then
   clang-format -i sobel.cpp sobel.cu sobel-cpu.cpp sobel-cpu-linear.cpp
 fi
 # g++ -std=c++1y -pedantic sobel-cpu.cpp -o cpu-image-filter
-g++ -std=c++1y -pedantic sobel-cpu-linear.cpp -o image-filter
+g++ -std=c++1y -pedantic sobel-cpu-linear.cpp -o cpu-image-filter
 chmod +x cpu-image-filter
 if hash nvcc 2>/dev/null; then
   nvcc --std=c++11 sobel.cpp sobel.cu -o gpu-image-filter
--- sobel-cpu-linear.cpp
+++ sobel-cpu-linear.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -91,7 +90,7 @@ int main(int argc, char *argv[]) {
   // extract image data, initialize vectors
   int src_rows = information.height + 2;
   int src_column_size = information.width + 2;
-  int src_size = rows * column_size;
+  int src_size = src_rows * src_column_size;
   int **data = (int **)malloc(src_size * sizeof(int *));
   for (row = 1; row <= information.height; row++) {
     data[row] = (int *)malloc(src_column_size * sizeof(int));
@@ -121,59 +120,57 @@ int main(int argc, char *argv[]) {
             << information.height << std::endl;
 
   // linear-ize source array
+  int dest_size = information.width * information.height;
   int *l_source_array = 0;
-  l_source_array = (int *)malloc(src_size * sizeof(int));
+  l_source_array = new int[src_size];
   for (row = 0; row < src_rows; row++) {
     for (col = 0; col < src_column_size; col++) {
-      l_source_array[row * src_column_size + col] = source_array[row][col];
+      l_source_array[row * src_column_size + col] = data[row][col];
     }
   }
 
-  int dest_size = information.width * information.height;
-  int *l_source_array = 0;
-  l_source_array = (int *)malloc(dest_size * sizeof(int));
-
-  int x_0, x_1, x_2, x_3, x_5, x_6, x_7, x_8, sum_0, sum_1;
+  int result_rows = information.height;
+  int result_column_size = information.width;
+  int *l_result_array = 0;
+  l_result_array = new int[dest_size];
   for (row = 1; row < (information.height + 1); row++) {
-    l_source_array[row - 1] = (int *)malloc(information.width * sizeof(int));
     for (col = 1; col < (information.width + 1); col++) {
       sobel(l_source_array, l_result_array, src_rows, src_column_size, row,
             col);
     }
   }
-}
 
-// de-linearize result array
-int **newData = (int **)malloc(dest_size * sizeof(int *));
-for (row = 0; row < result_row_size; row++) {
-  newData[row] = (int *)malloc(result_column_size * sizeof(int));
-  for (col = 0; col < result_column_size; col++) {
-    newData[row][col] = l_result_array[(row * result_column_size) + col];
+  // de-linearize result array
+  int **newData = (int **)malloc(dest_size * sizeof(int *));
+  for (row = 0; row < result_rows; row++) {
+    newData[row] = new int[result_column_size];
+    for (col = 0; col < result_column_size; col++) {
+      newData[row][col] = l_result_array[(row * result_column_size) + col];
+    }
   }
-}
 
-// write header to new image file
-newImageFile.write((char *) & header, sizeof(header_type));
-newImageFile.write((char *) & information, sizeof(information_type));
+  // write header to new image file
+  newImageFile.write((char *)&header, sizeof(header_type));
+  newImageFile.write((char *)&information, sizeof(information_type));
 
-// write new image data to new image file
-for (row = 0; row < information.height; row++) {
-  for (col = 0; col < information.width; col++) {
-    tempData[0] = (unsigned char)newData[row][col];
-    tempData[1] = (unsigned char)newData[row][col];
-    tempData[2] = (unsigned char)newData[row][col];
-    newImageFile.write((char *)tempData, 3 * sizeof(unsigned char));
+  // write new image data to new image file
+  for (row = 0; row < information.height; row++) {
+    for (col = 0; col < information.width; col++) {
+      tempData[0] = (unsigned char)newData[row][col];
+      tempData[1] = (unsigned char)newData[row][col];
+      tempData[2] = (unsigned char)newData[row][col];
+      newImageFile.write((char *)tempData, 3 * sizeof(unsigned char));
+    }
+    if (padding) {
+      tempData[0] = 0;
+      tempData[1] = 0;
+      tempData[2] = 0;
+      newImageFile.write((char *)tempData, padding * sizeof(unsigned char));
+    }
   }
-  if (padding) {
-    tempData[0] = 0;
-    tempData[1] = 0;
-    tempData[2] = 0;
-    newImageFile.write((char *)tempData, padding * sizeof(unsigned char));
-  }
-}
-std::cout << newImageFileName << " done." << std::endl;
-imageFile.close();
-newImageFile.close();
+  std::cout << newImageFileName << " done." << std::endl;
+  imageFile.close();
+  newImageFile.close();
 
-return 0;
+  return 0;
 }
