58173542cbb024a7d8aaaab74ff0ac92d401199b
Brett VanderHaar <brett.vanderhaar@gmail.com>
2015-11-19 22:37:39 -0500

    compile

--- sobel.cu
+++ sobel.cu
@@ -54,9 +54,9 @@ __global__ void cu_sobel(int *l_source_array_d, int *l_result_array_d, int rows,
 // Called from driver program.  Handles running GPU calculation
 extern "C" void gpu_sobel(int *l_source_array, int *l_result_array,
                           int src_rows, int src_column_size) {
-  int row, col;
   int num_bytes_source = src_column_size * src_rows * sizeof(int);
   int *l_source_array_d;
+  int *l_result_array_d;
 
   gpuErrchk(cudaMalloc((void **)&l_source_array_d, num_bytes_source));
   gpuErrchk(cudaMemcpy(l_source_array, l_source_array_d, num_bytes_source,
@@ -64,10 +64,8 @@ extern "C" void gpu_sobel(int *l_source_array, int *l_result_array,
 
   int result_column_size = src_column_size - 2;
   int result_row_size = src_rows - 2;
-  int num_bytes_result = result_column_size * result_column_size * sizeof(int);
-  int *l_result_array = 0;
-  int *l_result_array_d = 0;
-  l_result_array = (int *)malloc(num_bytes_result);
+  int num_bytes_result = result_column_size * result_row_size * sizeof(int);
+  //l_result_array = (int *)malloc(num_bytes_result);
   gpuErrchk(cudaMalloc((void **)&l_result_array_d, num_bytes_result));
   gpuErrchk(cudaMemcpy(l_result_array, l_result_array_d, num_bytes_result,
                        cudaMemcpyHostToDevice));
